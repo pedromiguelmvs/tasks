9055ebe90bef832d128717eb6506e241976846dd
miguelito ツ <pedromiguelmvs>
2024-03-05 12:09:09 -0300

    core(mapping): adiciona novos mapeamentos

---
+++ Api/Common/Mappers/MappingProfile.cs
@@ -0,0 +1,26 @@
+using Api.Modules.AppTasks;
+using Api.Modules.Auth;
+using Api.Modules.Users;
+using AutoMapper;
+
+public class MappingProfile : Profile
+{
+  public MappingProfile()
+  {
+    CreateMap<CreateAppTaskDto, AppTaskDto>().ReverseMap();
+    CreateMap<CreateAppTaskDto, AppTask>().ReverseMap();
+    CreateMap<AppTask, AppTaskDto>().ReverseMap();
+    CreateMap<User, UserDto>().ReverseMap();
+
+    CreateMap<UserDto, RegisterDto>().ReverseMap();
+    CreateMap<UserDto, AuthDto>().ReverseMap();
+
+    CreateMap<RegisterDto, CreateUserDto>().ReverseMap();
+
+    CreateMap<CreateUserDto, User>();
+    CreateMap<UpdateUserDto, User>();
+
+    CreateMap<string, TokenDto>()
+            .ForMember(dest => dest.Token, opt => opt.MapFrom(src => src));
+  }
+}
